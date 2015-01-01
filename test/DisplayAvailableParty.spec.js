c149d7abaaaa7b2068b03aed83d0383ebed3c404
Giorgia <giorgia1london@gmail.com>
2015-01-01 11:50:17 +0000

    failure test

--- test/DisplayAvailableParty.spec.js
+++ test/DisplayAvailableParty.spec.js
@@ -1,15 +1,21 @@
-describe('DisplayAvailableParty', function() {
+describe('Controllers', function() {
   beforeEach(module('partyApp'));
 
   var ctrl, scope;
 
   beforeEach(inject(function($rootScope, $controller){
     scope = $rootScope.$new();
-    ctrl = $controller('DisplayAvailableParty', {
-    	$scope: scope
-    });
+    ctrl = $controller('DisplayAvailableParty', {$scope: scope});
+    // expect($scope.party).toBeDefined();
   }));
-})
 
-// Because controllers are not available on the global scope, we need to use angular.mock.inject
-//  to inject our controller first. The first step is to use the module function,
+  describe('DisplayAvailableParty', function(){
+  	it('has a controller', function(){
+  		expect(ctrl).toBeDefined();
+  	})
+  })
+	// it('has a name', function(){
+	// 	expect($scope).not.toBeUndefined();
+	// });
+
+});
