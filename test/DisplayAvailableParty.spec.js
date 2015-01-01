6b676976a1bb6e3ec28073dcf6d68dc5d7264cf8
Giorgia <giorgia1london@gmail.com>
2015-01-01 12:37:56 +0000

    dummy tests working

--- test/DisplayAvailableParty.spec.js
+++ test/DisplayAvailableParty.spec.js
@@ -9,12 +9,30 @@ describe('Controllers', function() {
   }));
 
   describe('DisplayAvailableParty', function(){
-  	it('has a controller', function(){
+  	it('is a controller', function(){
   		expect(ctrl).toBeDefined();
+  		expect(scope).toBeDefined();
   	})
-  })
-	// it('has a name', function(){
-	// 	expect($scope).not.toBeUndefined();
-	// });
 
+		it('party is a defined object', function(){
+			expect(scope.party).not.toBeUndefined();
+			// console.log(scope.party)
+		});
+
+		it('has a name', function(){
+			expect(scope.party.name).toBe('Giorgia');
+		});
+
+		it('has an organizer', function(){
+			expect(scope.party.organizer).toBe('IDUNNO');
+		});
+
+		it('has a date', function(){
+			expect(scope.party.location).toBe('London');
+		});
+
+		it('has a venue date', function(){
+			expect(scope.party.date).toBe('1/1/2013');
+		});
+  })
 });
