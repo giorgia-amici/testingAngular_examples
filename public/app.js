35286572c7d8a853b932e73cb1123a3cf768a8c8
Giorgia <giorgia1london@gmail.com>
2015-01-02 11:16:25 +0000

    ngRoute

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
-var partyApp = angular.module('partyApp', []);
+var partyApp = angular.module('partyApp', ['ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider.when('/NewParty',
+		{
+			template.Url:'templates/NewParty.html',
+			controller: 'CreateNewParty'
+		})
+	});
